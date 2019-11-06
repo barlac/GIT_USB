ac3a5a1c31fbd7d14b81cf413eaa719594e6799e
Lachlan B <barneslachlan@gmail.com>
2019-11-06 22:50:50 +1300

    Code: Added some comments

--- Code/GIT_USB.py
+++ Code/GIT_USB.py
@@ -1,6 +1,19 @@
 import os
 from git import Repo
 
+def get_merge_results(file_list):
+    # Freeman to potentially implement.
+    # Description:
+    # Use PythonQt to generate a GUI and allow users to select for each of
+    # the files with merge conflicts if they want to keep their version or the incoming
+    # version.
+
+    # Input: [0] file_list = A list of file names with merge conflicts.
+    # Outputs [0] ...to be named... = A dictionary of file names and status of if the 
+    # current changed will be kept or overwritten by incoming changes.
+    # *** I/O types can change easily, just a sugestion :) ***
+    return {'fileA': True, 'fileB': False, 'fileC': True}
+
 def behind_master(repo):
     commits_behind_gen = repo.iter_commits('master..origin/master')
     commits_behind = sum(1 for c2 in commits_behind_gen)
