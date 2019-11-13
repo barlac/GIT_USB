7180d9daabd9ebd0c3a415579567c19d111d04a5
Lachlan B <barneslachlan@gmail.com>
2019-11-13 16:33:35 +1300

    Code: Added the flowcharts to the repo and implemented the sempahore

--- Code/GIT_USB.py
+++ Code/GIT_USB.py
@@ -2,20 +2,28 @@ import os
 from git import Repo
 import threading
 
-def get_merge_results(file_list):
-    # Freeman to potentially implement.
-    # Description:
-    # Use PythonQt to generate a GUI and allow users to select for each of
-    # the files with merge conflicts if they want to keep their version or the incoming
-    # version.
+# Global semaphore to ensure two sync folders instances are not called 
+semaphore = False
 
-    # Input: [0] file_list = A list of file names with merge conflicts.
-    # Outputs [0] ...to be named... = A dictionary of file names and status of if the 
-    # current changed will be kept or overwritten by incoming changes.
-    # *** I/O types can change easily, just a sugestion :) ***
+def get_merge_results(file_list):
+    """
+    Freeman to potentially implement.
+    Description:
+    Use PythonQt to generate a GUI and allow users to select for each of
+    the files with merge conflicts if they want to keep their version or the incoming
+    version.
+
+    Input: [0] file_list = A list of file names with merge conflicts.
+    Outputs [0] ...to be named... = A dictionary of file names and status of if the 
+    current changed will be kept or overwritten by incoming changes.
+    *** I/O types can change easily, just a sugestion :) ***
+    """
     return {'fileA': True, 'fileB': False, 'fileC': True}
 
 def behind_master(repo):
+    """
+    Return a True/False if the local repo is behind master or not
+    """
     commits_behind_gen = repo.iter_commits('master..origin/master')
     commits_behind = sum(1 for c2 in commits_behind_gen)
     # commits_ahead_gen = repo.iter_commits('origin/master..master')
@@ -23,6 +31,9 @@ def behind_master(repo):
     return bool(commits_behind)
 
 def uncommitted_changes(repo):
+    """
+    Return a True/False if the local repo has uncommitted changes or not
+    """
     changedFiles = [ item.a_path for item in repo.index.diff(None) ]
     untrackedFiles = repo.untracked_files
     return bool(len(changedFiles) + len(untrackedFiles))
@@ -32,18 +43,31 @@ def stage_and_commit_all(repo):
     return 1
 
 def sync_folder(repo):
+    """
+    Implements the flowchart on GitHub and when called syncs the "Shared_Folder" with another other users
+    """
     print('Starting Sync!')
     commit_needed = uncommitted_changes(repo)
     master_ahead = behind_master(repo)
     if(master_ahead & commit_needed):
         stage_and_commit_all(repo)
+    return False
 
 def mytimer(repo):
-    # calls the sync folder function every X seconds
+    """
+    Timer object that creates a thread to call the sync folder function every X seconds.
+    Uses the global semaphore to ensure that only one instance of the sync_folder is created.
+    """
+    global semaphore
     threading.Timer(3.0, mytimer, args=(repo,)).start()
-    sync_folder(repo)
+    if(not semaphore):
+        semaphore = True
+        semaphore = sync_folder(repo)
 
 def main():
+    """
+    Function Main
+    """
     #Create repo object
     repo = Repo(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "Shared_Folder"))
     threading.Timer(3.0, mytimer, args=(repo,)).start()
